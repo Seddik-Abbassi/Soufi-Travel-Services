b21c7481ddc668f3aaabdbfb3ee9c573209a45ef
Seddik-Abbassi <abbassi.seddik@gmail.com>
2021-05-11 18:23:05 +0100

    Revert "ferry formulaire"
    This reverts commit 39052dd4d22c20663e6af120236364e9c105cb14.

--- public/js/style.js
+++ public/js/style.js
@@ -1,12 +1,6 @@
 $( document ).ready(function() {
-
-    $('.cs-select ul li').click(function (){
-        var moyen = $(this).data("value");
-
-        if(moyen === 'Passager piéton' || moyen === 'Vélo' || moyen === 'Moto') {
-            $('#markpart').removeClass('hidden').addClass('hidden');
-        }else {
-            $('#markpart').removeClass('hidden');
-        }
+    // $("#markpart").removeClass("hidden");
+    $(document).on('change','#moyenid',function(){
+        alert('Change Happened');
     });
 });
